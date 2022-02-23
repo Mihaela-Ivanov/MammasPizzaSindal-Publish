eec0524afec3c92f403d8abd08d972fbd5471184
Mihaela Ivanov <CW40DH@its.aau.dk>
2022-02-23 19:12:06 +0100

    Add project files.

---
+++ ViewModels/Group.cs
@@ -0,0 +1,12 @@
+﻿using MammasPizzaSindal.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MammasPizzaSindal.Models
+{
+    public class Group<K, T>
+    {
+        public K Key { get; set; }
+        public IEnumerable<T> Values { get; set; }
+    }
+}
